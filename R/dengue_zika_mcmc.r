ce8728ef911897f361b6abfab75ab8825fcbf8b0
sbfnk <sbfnk@users.noreply.github.com>
2016-07-07 08:27:46 +0100

    floor burn

--- R/dengue_zika_mcmc.r
+++ R/dengue_zika_mcmc.r
@@ -365,7 +365,7 @@ final_model <- bi_wrapper$model
 res <- bi_read(read = bi_wrapper, thin = thin, verbose = verbose)
 
 ## 25% burn-in
-burn <- num_samples / thin * 0.25
+burn <- floor(num_samples / thin * 0.25)
 dic <- compute_DIC(read = res, burn = burn)
 
 cat("DIC: ", dic, "\n")
