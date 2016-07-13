74a679d45c167a18d5b6e199726d16232568be7e
sbfnk <sbfnk@users.noreply.github.com>
2016-07-13 23:29:47 +0100

    better parallel runs

--- R/analyse_parallel_runs.r
+++ R/analyse_parallel_runs.r
@@ -3,22 +3,41 @@ library('RBi.helpers')
 library('coda')
 library('dplyr')
 
+output_dir <- path.expand("~/Data/Zika")
+
+mcmc_files <- list.files(output_dir, pattern = "vbd_poisson_sero_patch_[0-9]+\\.rds", full.names = TRUE)
+
 traces <- list()
-Z_traj <- list()
-for (i in 1:20)
+
+for (file in mcmc_files)
 {
-    filename <-
-        paste("~/Data/Zika/vbd_beta_sero", i, sep = "_")
-    res_filename <- paste(filename, "rds", sep = ".")
-    model_filename <- paste(filename, "bi", sep = ".")
-    if (file.exists(res_filename) && file.exists(model_filename)) 
-    {
-      res <- readRDS(res_filename)
-      model <- bi_model(model_filename)
-      traces[[i]] <- mcmc(get_traces(res, model = model))
-      Z_traj[[i]] <- res[["Z_h"]]
-    }
+    traces[[file]] <- readRDS(file)
 }
+
+np_translate <- NULL
+combined <- lapply(names(traces[[1]]), function(x) {
+  z <- rbindlist(lapply(seq_along(traces), function(y) {
+    data.table(traces[[y]][[x]])[, unique_np := paste(np, y, sep = "_")]
+  }))
+  if (is.null(np_translate)) {
+    np_translate <-
+      data.table(unique_np = unique(z$unique_np),
+                 new_np = seq_along(unique(z$unique_np)) - 1)
+  }
+  z <- merge(z, np_translate, by = "unique_np")
+  z[, np := NULL]
+  z[, unique_np := NULL]
+  setnames(z, "new_np", "np")
+  setkey(z, np)
+  z
+})
+
+
+model <- bi_model("~/code/vbd/bi/vbd_patch.bi")
+names(combined) <- names(traces[[1]])
+
+p <- plot_libbi(combined, model, extra.aes = c(color = "disease", linetype = "setting"))
+
 
 res <- list(Z_h = bind_rows(Z_traj))
 
