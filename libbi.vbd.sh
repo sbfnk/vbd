4fefcea1e69894eb131c842f1b8510f8efa37c91
sbfnk <sbfnk@users.noreply.github.com>
2016-07-13 13:13:59 +0100

    merge

--- libbi.vbd.sh
+++ libbi.vbd.sh
@@ -3,7 +3,7 @@
 #$ -V -cwd
 #$ -M sebastian.funk@lshtm.ac.uk
 #$ -l mem_free=2000M,h_vmem=8000M
-#$ -pe smp 4
+#$ -pe smp 8
 #$ -q parallel.q
 #$ -R y
 #$ -t 1-100
@@ -12,8 +12,5 @@ seed=$RANDOM
 
 echo Seed: $seed
 
-##Rscript ~/code/vbd/R/dengue_zika_mcmc.r -n 10000000 -p 10000 -t 4 -e $seed -a -i 1000 -f -r -l 
-##Rscript ~/code/vbd/R/dengue_zika_mcmc.r -n 50000 -p 10000 -a -i  1000 -r -l -k -f -v -e 11724 -c 32
-Rscript ~/code/vbd/R/dengue_zika_mcmc.r --nsamples 100000 --pre_samples 10000 --seed $seed --sample-prior --sample-observations --keep --force --thin 100 --sero --beta
 Rscript ~/code/vbd/R/dengue_zika_mcmc.r --nsamples 1000000 --pre_samples 1000 --sample-prior --sample-observations  --force --thin 1000 --patch --poisson --seed $seed --parallel-number ${SGE_TASK_ID}
 
