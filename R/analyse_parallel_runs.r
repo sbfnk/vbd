1d6d2803e3d5241050948a93c0d4286c09f58fca
sbfnk <sbfnk@users.noreply.github.com>
2016-09-09 15:17:06 +0100

    save variables

--- R/analyse_parallel_runs.r
+++ R/analyse_parallel_runs.r
@@ -501,6 +501,7 @@ ggsave("GI_R0.pdf", p, width = 7, height = 2.3)
 
 two_panels <- plot_grid(p_r0gi[["vbd_fnh"]]$densities + xlab(""), p,
                         ncol = 2, labels = c("A", "B"))
+save_plot("r0gi_two.pdf", two_panels, ncol = 2)
 
 ggsave("dengue_zika_obs.pdf", p_obs[["vbd_fnh"]], width = 7, height = 3)
 
@@ -594,14 +595,21 @@ for (model in grep("_earlier$", models, invert = TRUE, value = TRUE))
         theme(legend.position = "top")
 }
 
-saveRDS(list(r0_gi = r0_gi,
-             all_params = all_params,
-             r0_gi_summary = r0_gi_summary,
-             p_r0vgi = p_r0vgi,
-             p_libbi = p_libbi,
-             p_r0 = p_r0,
-             p_r0gi = p_r0gi,
-             p_r0_sqrt = p_r0_sqrt),
-        "results.rds")
+p_densities <- lapply(p_libbi, function(x) { x[["posterior"]][["densities"]]})
 
-save_plot("r0gi_two.pdf", two_panels, ncol = 2)
+save_vars <- list(r0_gi = r0_gi,
+                  all_params = all_params,
+                  r0_gi_summary = r0_gi_summary,
+                  p_densities = p_densities, 
+                  p_r0vgi = p_r0vgi,
+                  p_r0 = p_r0,
+                  p_r0gi = p_r0gi,
+                  p_r0_sqrt = p_r0_sqrt)
+
+saveRDS(save_vars, "results.rds")
+## save_vars <- readRDS("results.rds")
+
+for (name in names(save_vars))
+{
+    assign(name, save_vars[[name]])
+}
